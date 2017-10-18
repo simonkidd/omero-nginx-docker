0544b0288f6bde1a12ff4e51756aac270cdee000
dpwrussell <douglas_russell@hms.harvard.edu>
2017-10-18 16:06:45 -0400

    Fix use of nginx without microservices

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 set -eu
 
 OMEROWEBHOST=${OMEROWEBHOST:-web}
+OMERO_MS_THUMBNAIL_BACKEND_HOST=${OMERO_MS_THUMBNAIL_BACKEND_HOST:-}
+OMERO_MS_IMAGE_REGION_BACKEND_HOST=${OMERO_MS_IMAGE_REGION_BACKEND_HOST:-}
 
 sed -i -re "s/(server )[a-zA-Z0-9][a-zA-Z0-9.-]*[a-zA-Z0-9](:4080 fail_timeout=0;)/\1$OMEROWEBHOST\2/" /etc/nginx/conf.d/omero-web.conf
 sed -i -re 's/warn/debug/' /etc/nginx/nginx.conf
